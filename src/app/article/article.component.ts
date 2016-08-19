0751fac457a4871726f16b8762fde5501ee31a9b
Will <doggy.huang@gmail.com>
2016-08-19 10:55:43 +0800

    59 練習元件之間的訊息傳遞方法：@Output()

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-article',
@@ -10,9 +10,15 @@ export class ArticleComponent implements OnInit {
   @Input() i:any;
   @Input() item: any;
 
+  @Output()
+  delete = new EventEmitter<number>();
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  deleteArticle(i) {
+    this.delete.emit(i);
+  }
 }
